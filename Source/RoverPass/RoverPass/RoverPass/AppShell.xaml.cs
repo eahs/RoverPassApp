09d7b42e78118cfcba518f39f9dc1da17d55b9d3
Michael Tanczos <tanczosm@eastonsd.org>
2020-11-02 09:25:07 -0500

    RoverPass Initial Boilerplate code

---
+++ Source/RoverPass/RoverPass/RoverPass/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using RoverPass.ViewModels;
+using RoverPass.Views;
+using Xamarin.Forms;
+
+namespace RoverPass
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ Source/RoverPass/RoverPass/RoverPass/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using RoverPass.Models;
+using RoverPass.ViewModels;
+
+namespace RoverPass.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
