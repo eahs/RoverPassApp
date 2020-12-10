db35fcfea36a4338ce703598986fd2d3a394e771
Taranveer <robinflora0529@gmail.com>
2020-12-09 20:21:32 -0500

    Added PassAprove

---
+++ Source/RoverPass/RoverPass/RoverPass/ViewModels/PassAprove/PassAproveViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using RoverPass.Models.PassAprove;
+
+namespace RoverPass.ViewModels.PassAprove
+{
+    /// <summary>
+    /// ViewModel for contacts page.
+    /// </summary>
+}
+    
---
+++ Source/RoverPass/RoverPass/RoverPass/Views/PassAproval/PassAprove.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+using RoverPass.DataService;
+
+namespace RoverPass.Views.PassAprove
+{
+    /// <summary>
+ 
+
+}
