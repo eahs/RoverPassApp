fd242973eb933f59c6ffde7e73df65216d2c1acd
Taranveer <robinflora0529@gmail.com>
2020-12-17 19:01:02 -0500

    Designed Pass Queue Layout and added temporary/fake data

--- Source/RoverPass/RoverPass/RoverPass/Views/QueuePage/PassQueue.xaml.cs
+++ Source/RoverPass/RoverPass/RoverPass/Views/QueuePage/PassQueue.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
@@ -14,11 +15,31 @@ namespace RoverPass.Views.QueuePage
         /// <summary>
         /// Initializes a new instance of the <see cref="T:RoverPass.Views.QueuePage.AboutUsSimplePage"/> class.
         /// </summary>
+        
+        public class Employee
+        {
+            public string DisplayName { get; set; }
+        }
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+        public ObservableCollection<Employee> Employees { get { return employees; } }
+
+        //  public object EmployeeView { get; }
+
         public PassQueue()
         {
             InitializeComponent();
+            //EmployeeView.ItemsSource = employees;
+
+            // ObservableCollection allows items to be added after ItemsSource
+            // is set and the UI will react to changes
+            employees.Add(new Employee { DisplayName = "Rob Finnerty" });
+            employees.Add(new Employee { DisplayName = "Bill Wrestler" });
+            employees.Add(new Employee { DisplayName = "Dr. Geri-Beth Hooper" });
+            employees.Add(new Employee { DisplayName = "Dr. Keith Joyce-Purdy" });
+            employees.Add(new Employee { DisplayName = "Sheri Spruce" });
+            employees.Add(new Employee { DisplayName = "Burt Indybrick" });
         }
 
-     
+
     }
 }
