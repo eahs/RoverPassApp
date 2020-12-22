c18e4609f613fd86bb9f08effa71d58954fa9378
IAISA <imaanshahzad.ali@gmail.com>
2020-12-21 20:34:34 -0500

    Created Hall Monitor
    Teacher SIde

---
+++ Source/RoverPass/RoverPass/RoverPass/Views/HallMonitor/HallMonitor.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace RoverPass.Views.HallMonitor
+{
+    /// <summary>
+    /// About us simple page.
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HallMonitor
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:RoverPass.Views.HallMonitor.AboutUsSimplePage"/> class.
+        /// </summary>
+        public HallMonitor()
+        {
+            InitializeComponent();
+        }
+    }
+}
