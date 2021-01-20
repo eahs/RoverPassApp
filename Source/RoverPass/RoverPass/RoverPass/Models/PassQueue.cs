568de1b89d65dcebfa865cb913c0d29e000db257
IAISA <imaanshahzad.ali@gmail.com>
2021-01-20 11:26:04 -0500

    Created PassQueue Model

---
+++ Source/RoverPass/RoverPass/RoverPass/Models/PassQueue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoverPass.Models
+{
+    class PassQueue
+    {
+        public string studentName { get; set; }
+        public string className { get; set; }
+        public string timeRemaining { get; set; }
+        public string signInTime { get; set; }
+        public string signOutTime { get; set; }
+
+    }
+}
